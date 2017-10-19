100ffa59182d9be369830706a1379041fc91e125
WNjihia <dorisn172@gmail.com>
2017-10-19 15:04:33 +0300

    define get, put and delete routes

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import BucketlistView
+from .views import BucketlistView, BucketlistDetailsView
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls',
                            namespace='rest_framework')),
     url(r'^bucketlists/$', BucketlistView.as_view(), name="create"),
-}
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDetailsView.as_view(),
+        name="bucketlist_details"),
+    }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
