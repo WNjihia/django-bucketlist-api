715de5c122b03adb3f7c20c5f5c376d56cfab454
WNjihia <dorisn172@gmail.com>
2017-10-19 15:03:46 +0300

    add bucketlist get, update and delete functionality

--- rest_api/views.py
+++ rest_api/views.py
@@ -12,9 +12,29 @@ class BucketlistView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         """Returns all bucketlists created by the current user."""
-        queryset = Bucketlist.objects.all()
+        queryset = Bucketlist.objects.filter(created_by=self.request.user)
         return queryset
 
     def perform_create(self, serializer):
         """Save the bucketlist details when creating a new bucketlist."""
         serializer.save(created_by=self.request.user)
+
+
+class BucketlistDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class retrieves, updates and deletes specific bucketlists."""
+
+    serializer_class = BucketlistSerializer
+
+    def get_queryset(self):
+        queryset = Bucketlist.objects.filter(created_by=self.request.user)
+        return queryset
+
+
+class ItemlistView(generics.ListCreateAPIView):
+    """This class creates and retrieves all bucketlist items."""
+    pass
+
+
+class ItemlistDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class retrieves, updates and deletes specific bucketlist items."""
+    pass
